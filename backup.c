e236a83d14080b419acd3134eb8bfe579d5f5b4a
diablo.fan <diablo.fan@libratone.com.cn>
2017-09-14 16:29:49 +0800

    fix new line \n

--- backup.c
+++ backup.c
@@ -1,5 +1,5 @@
 #ifdef DEBUG
-#define DEBUG_PRINT(format,...) printf("FILE: "__FILE__", LINE: %d: "format"/n", __LINE__, ##__VA_ARGS__)
+#define DEBUG_PRINT(format,...) printf("FILE: "__FILE__", LINE: %d: "format"\n", __LINE__, ##__VA_ARGS__)
 #else
 #define DEBUG_PRINT(format,...)
 #endif
